ed54e3ac9fe43e65f58a12ddadc5e40e5cdf97ea
Shambhavi K M <shambhavimahadev12@gmail.com>
2022-10-31 19:34:18 +0530

    initial commit

---
+++ src/model/IDataList.tsx
@@ -0,0 +1,13 @@
+export default interface IDataList {		// it is use to map our json data retrieve from backend technologies 
+    id: number;
+    product : string,
+    price : number,
+    payeeName : string,
+    setDate : string
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
